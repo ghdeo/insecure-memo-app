42b07980c9f9c6e0bf7c30c76e5d8e261e2c2ae5
ghdeo <goldencorn7@naver.com>
2023-05-16 01:49:32 +0900

    feat: Add IndexActivity

--- app/src/main/java/com/hongik/insecurememo/IndexActivity.java
+++ app/src/main/java/com/hongik/insecurememo/IndexActivity.java
@@ -1,4 +1,30 @@
 package com.hongik.insecurememo;
 
-public class IndexActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IndexActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goMainActivity();
+            }
+        }, 2000);
+    }
+
+    public void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
