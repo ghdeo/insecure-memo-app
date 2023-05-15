82ca53395f21b2391b1903df99bd2d9999354ddd
ghdeo <goldencorn7@naver.com>
2023-05-16 01:48:26 +0900

    feat: Add MemoItem

--- app/src/main/java/com/hongik/insecurememo/item/MemoItem.java
+++ app/src/main/java/com/hongik/insecurememo/item/MemoItem.java
@@ -1,4 +1,20 @@
 package com.hongik.insecurememo.item;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MemoItem {
+    public String category;
+    public String memo;
+    public String regDate;
+
+    public MemoItem(String category, String memo) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+        Date date = new Date();
+
+        this.category = category;
+        this.memo = memo;
+        this.regDate = format.format(date);
+    }
 }
